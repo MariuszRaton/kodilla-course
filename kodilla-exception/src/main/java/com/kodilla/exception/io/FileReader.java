ca5d651bdbe0e8139197b5970e3106e362deaad3
Mariusz Ratoń <raton.mariusz@gmail.com>
2019-07-20 09:50:43 +0200

    Add 8.3

--- kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
@@ -9,7 +9,7 @@ import java.util.stream.Stream;
 
 public class FileReader {
 
-    public void readFile() {
+    public void readFile() throws FileReaderException {
 
     ClassLoader classLoader = getClass().getClassLoader();
     File file = new File(classLoader.getResource("file/names.txt").getFile());
@@ -22,7 +22,9 @@ public class FileReader {
 
     } catch (IOException e) {
 
-        System.out.println("Błąd! Coś poszło nie tak! " + e);
+        throw new FileReaderException();
+
+       // System.out.println("Błąd! Coś poszło nie tak! " + e);
     } finally {
         System.out.println("I am gonna be here... always!");
     }
---
+++ kodilla-exception/src/test/java/com/kodilla/exception/test/ExceptionHandling.java
@@ -0,0 +1,19 @@
+package com.kodilla.exception.test;
+
+public class ExceptionHandling {
+
+    public static void main(String[] args) {
+
+        SecondChallenge secondChallenge = new SecondChallenge();
+
+        try {
+          //  secondChallenge.probablyIWillThrowException(1.5,1);
+            secondChallenge.probablyIWillThrowException(2,1.5);
+        } catch (Exception e) {
+            System.out.println("Wprowadzono błędne parametry. Błąd: " + e);
+
+        } finally {
+           System.out.println("Test SecondChallenge");
+        }
+    }
+}
